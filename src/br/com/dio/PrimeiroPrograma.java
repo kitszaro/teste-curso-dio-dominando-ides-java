0443789e5140f7887c47455e2721a5911bd621dd
Kitszaro <kitszaro@gmail.com>
2022-03-09 11:34:18 -0300

    Second Commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -3,8 +3,8 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
-    public static void main(String[] args){
-    Gato gato = new Gato();
+    public static void main(String[] args) {
+        Gato gato = new Gato();
 
         System.out.println(gato);
 
